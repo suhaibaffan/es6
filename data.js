dc1f633c84c627411a0ee960b8a9fc2f6dc70605
suhaibaffan <suhaibaffan91@gmail.com>
2017-11-22 22:59:44 +0530

    Data features in es6

---
+++ data.js
@@ -0,0 +1,20 @@
+/**
+ * New Number properties
+ */
+
+ Number.isInteger(1.05) //false
+
+ Number.isSafeInteger(number) 
+ Number.MAX_SAFE_INTEGER
+ Number.MIN_SAFE_INTEGER
+ Number.isNaN
+ Number.isFinite
+
+/**
+ * some new Math methods 
+ */
+Math.sign(-8) // -1
+Math.trunc(3.8) // 3
+Math.log10(100) // 2
+Math.hypot(3,4) //5 Computes the square root of the sum of the squares of its arguments 
+
